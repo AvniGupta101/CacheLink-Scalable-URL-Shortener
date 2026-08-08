b19b230d5e1f02f4ad183f4c0ed14b19daf4a607
Avni Gupta <avngupta06@gmail.com>
2026-08-08 16:12:23 +0530

    second

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,7 +2,7 @@ import { useState } from 'react';
 import axios from 'axios';
 import './App.css';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000'
+const API_URL = import.meta.env.VITE_API_URL || 'https://cache-link-scalable-url-shortener-q.vercel.app/'
 ;
 function App() {
   const [longUrl, setLongUrl] = useState('');
